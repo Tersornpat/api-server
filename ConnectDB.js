8bc5bb1b9e85b7c2fcb535ec6acf8cf687bd69cb
Tersornpat <sornpat@gmail.com>
2023-05-27 21:33:58 +0700

    add porapipat

--- ConnectDB.js
+++ ConnectDB.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 // Create MySQL connection
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'Porapipat',
+    password: 'Porapipat159753654',
     database: 'hospital_db',
 });
 
