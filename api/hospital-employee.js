a1b6b68b337417dabec1cb49ba7b195a83862751
Tersornpat <sornpat@gmail.com>
2023-05-29 02:11:28 +0700

    chang []

--- api/hospital-employee.js
+++ api/hospital-employee.js
@@ -65,7 +65,7 @@ router.get('/getempis/:positionName', (req, res) => {
             } else if (results.length === 0) {
                 res.status(404).json({ error: 'Employee not found' });
             } else {
-                res.status(200).json(results[0]);
+                res.status(200).json(results);
             }
         }
     );
