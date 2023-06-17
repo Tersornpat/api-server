8c94b191b4afedd37db19e01e8bd10526bb1f049
Tersornpat <sornpat@gmail.com>
2023-06-17 17:27:39 +0700

    add *

--- ConnectDB.js
+++ ConnectDB.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql');
 
-// Create MySQL connection
+// Create MySQL connection Localhost
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'hospital_db-temp',
+// });
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'admin',
+    password: 'c68c7ae898b3314ce258b9b4ad52d2321b435a18ba3a8116',
     database: 'hospital_db-temp',
 });
 
